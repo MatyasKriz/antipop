8732ff07df3e2d4f29f22d9c148e85e778c7a28c
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2018-10-11 20:54:23 +0200

    Initial Commit

---
+++ antipop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  antipop
+//
+//  Created by Matyáš Kříž on 11/10/2018.
+//  Copyright © 2018 Spark Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
